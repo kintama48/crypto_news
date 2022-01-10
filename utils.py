b5a6043b949da655da93f404be19ac663b906b8c
Abdullah Baig <65843525+kintama48@users.noreply.github.com>
2022-01-10 22:57:06 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,9 +13,9 @@ def news_helper(news):
 
 
 def create_telegram_msg(news):
-    text = f"*{news['title']}*" \
-           f"\n\n_{news['body']}_" \
-           f"\n\n*Source:* [Click here to visit the article site.]({news['guid']})"
+    text = f"\*{news['title']}*" \
+           f"\n\n\_{news['body']}_" \
+           f"\n\n\*Source:* \[Click here to visit the article site.]\({news['guid']})"
     return text
 
 
