1ce338a8df36e1827e86bc4e6727cb90b2c99a83
Abdullah Baig <65843525+kintama48@users.noreply.github.com>
2022-01-10 23:56:53 +0500

    Update __init__.py

--- telegram/ext/__init__.py
+++ telegram/ext/__init__.py
@@ -1,24 +1,3 @@
-#!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2022
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
-# pylint: disable=C0413
-"""Extensions over the Telegram Bot API to facilitate bot making"""
-
 from .extbot import ExtBot
 from .basepersistence import BasePersistence
 from .picklepersistence import PicklePersistence
@@ -28,9 +7,6 @@ from .callbackcontext import CallbackContext
 from .contexttypes import ContextTypes
 from .dispatcher import Dispatcher, DispatcherHandlerStop, run_async
 
-# https://bugs.python.org/issue41451, fixed on 3.7+, doesn't actually remove slots
-# try-except is just  here in case the __init__ is called twice (like in the tests)
-# this block is also the reason for the pylint-ignore at the top of the file
 try:
     del Dispatcher.__slots__
 except AttributeError as exc:
